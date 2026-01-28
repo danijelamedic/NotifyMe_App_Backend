d793dda0a63e24d93b9555f61c118a9a4b4ea2dd
Danijela Medic <medic.danijeladaca@gmail.com>
2026-01-28 17:45:39 +0100

    init: NotifyMe backend (for ISA Jutjubic App)

---
+++ src/main/java/com/notifyme/isa/notifyme_backend/NotifymeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.notifyme.isa.notifyme_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifymeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifymeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notifyme/isa/notifyme_backend/NotifymeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifyme.isa.notifyme_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifymeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
